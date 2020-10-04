882f8992ca850dc823cdbe7631d10d080a5ef630
Athi Dharma <artidharma@googlemail.com>
2020-10-04 13:32:45 +0100

    Added models for Bonds, Added Bonds config to installed apps

--- origin/bonds/models.py
+++ origin/bonds/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Bonds(models.Model):
+    isin = models.CharField(max_length=200)
+    size = models.IntegerField()
+    currency = models.CharField(max_length=200)
+    maturity = models.DateField()
+    lei = models.CharField(max_length=200)
+    legal_name = models.CharField(max_length=200)
